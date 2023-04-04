cb4417deafe298fe568a6c188e685274073d8f82
Nicholas <lynick14@gmail.com>
2023-04-04 15:56:17 -0400

    set up webpack

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+const App = props => {
+    return(
+    <div>
+        <h1>Solo Project</h1>
+    </div>
+    )
+}
+
+// class App extends Component {
+
+//     render() {
+//         return(
+//             <div>
+//                 <h1>Solo Project</h1>
+//             </div>
+//         )
+//     }
+
+// }
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // REACT DOM 
+import App from './Components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
